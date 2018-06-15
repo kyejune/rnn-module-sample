c339db70eaa0eac453eac015b034a5000cc4e1bd
KyeJune Hong <hohoyaing@gmail.com>
2018-06-15 16:53:02 +0900

    v0.1.3

--- RnnModuleSample/RnnModuleSample/RnnModuleSample.h
+++ RnnModuleSample/RnnModuleSample/RnnModuleSample.h
@@ -1,13 +1,13 @@
 //
 //  RnnModuleSample.h
-//  rnn-module-sample
+//  RnnModuleSample
 //
 //  Created by KyeJune Hong on 15/06/2018.
 //  Copyright © 2018 KyeJune Hong. All rights reserved.
 //
 
-#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
 
-@interface RnnModuleSample : NSObject <RCTBridgeModule>
+@interface RnnModuleSample : NSObject
 
 @end
---
+++ RnnModuleSample/RnnModuleSample/RnnModuleSample.m
@@ -0,0 +1,13 @@
+//
+//  RnnModuleSample.m
+//  RnnModuleSample
+//
+//  Created by KyeJune Hong on 15/06/2018.
+//  Copyright © 2018 KyeJune Hong. All rights reserved.
+//
+
+#import "RnnModuleSample.h"
+
+@implementation RnnModuleSample
+
+@end
