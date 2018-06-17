d2b685208800a50d8e08f7a9d866f14747ed7d57
KyeJune Hong <hohoyaing@gmail.com>
2018-06-17 14:17:47 +0900

    add document

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # rnn-module-sample
+
+
+### Document 및 참고 URL
+1. https://facebook.github.io/react-native/docs/native-modules-ios.html
+   https://facebook.github.io/react-native/docs/native-modules-android.html
+
+2. https://medium.com/@kyo504/npm%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-react-native-%EB%84%A4%EC%9D%B4%ED%8B%B0%EB%B8%8C-%EB%AA%A8%EB%93%88-%EC%9E%91%EC%84%B1%EA%B3%BC-%EB%B0%B0%ED%8F%AC-672533dabea9
+
+
+
+### Flow
+```
+1. 깃헙저장소 생성 및 연결, npm init 세팅
+2. xcode project, android 폴더 구조 생성 ( 2번 URL 참고 )
+3. 각각 프로젝트에서 필요한 함수 및 속성을 설정
+4. react-native 에서 git주소로 해당 npm 모듈을 추가해서 사용
+```
+
+
+
+
--- ios/RnnModuleSample/RnnModuleSample.m
+++ ios/RnnModuleSample/RnnModuleSample.m
@@ -6,11 +6,22 @@
 
 @implementation RnnModuleSample
 
+// 필수 해당 모듈네임으로 react-native에서 사용할 수 있게 추출
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(getModuleList: (RCTResponseSenderBlock)callback)
+
+// rn -> n 호출: RCT_EXPORT_METHOD( 사용할 함수명:... )
+RCT_EXPORT_METHOD(callback: (RCTResponseSenderBlock)callback)
 {
-    NSArray *nativeModuleList = @[@"-----react-native-fbsdk", @"react-native-camera", @"react-native-maps"];
+    NSArray *nativeModuleList = @[@"call", @"back"];
     callback(@[[NSNull null], nativeModuleList]);
 }
+
+/**
+ RCT_EXPORT_MODULE: 함수 등록,
+ RCT_REMAP_METHOD: 프로미스 구현,
+ RCTEventEmitter: 이벤트 전송
+ **/
+
+
 @end
